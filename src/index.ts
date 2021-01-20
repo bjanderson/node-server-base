e849e1e8d0bfbf88b546c42be4162e46855d0775
BJ Anderson <andersonb79@gmail.com>
2021-01-20 18:37:45 -0500

    updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,15 @@ import path from 'path';
 import { router } from './routes';
 import { setJsonHeaders, unknownEndpoint } from './routes/util-middleware';
 
-const httpsPort = 3443;
-const pathToKey = path.resolve('certs', 'server.key');
-const pathToCert = path.resolve('certs', 'server.crt');
-const pathToDHStrong = path.resolve('certs', 'dh-strong.pem');
-const options: ServerOptions = {
-  key: readFileSync(pathToKey),
-  cert: readFileSync(pathToCert),
-  dhparam: readFileSync(pathToDHStrong),
-};
+// const httpsPort = 3443;
+// const pathToKey = path.resolve('certs', 'server.key');
+// const pathToCert = path.resolve('certs', 'server.crt');
+// const pathToDHStrong = path.resolve('certs', 'dh-strong.pem');
+// const options: ServerOptions = {
+//   key: readFileSync(pathToKey),
+//   cert: readFileSync(pathToCert),
+//   dhparam: readFileSync(pathToDHStrong),
+// };
 
 const server = express();
 
@@ -32,11 +32,11 @@ server.use('/api', setJsonHeaders, router);
 
 server.use(unknownEndpoint);
 
-// const httpPort = 3001;
-// server.listen(httpPort, () => {
-//   console.log(`Server is running at http://localhost:${httpPort}`);
-// });
-
-createServer(options, server).listen(httpsPort, () => {
-  console.log(`Server is running at https://localhost:${httpsPort}`);
+const httpPort = 3001;
+server.listen(httpPort, () => {
+  console.log(`Server is running at http://localhost:${httpPort}`);
 });
+
+// createServer(options, server).listen(httpsPort, () => {
+//   console.log(`Server is running at https://localhost:${httpsPort}`);
+// });
